e1ba9d59ef62f855d685606aa5d518f22bf5f60e
flamestro <NoReplyFlamestro@gmail.com>
2020-11-21 18:31:11 +0100

    add delete button styling

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,5 +1,5 @@
 import {Card} from "../../App/App.models";
-import React from "react";
+import React, {useState} from "react";
 import styled from "styled-components";
 
 export interface CardProps {
@@ -9,13 +9,38 @@ export interface CardProps {
 const StyledCard = styled.div`
     border-bottom: 1px solid black;
     padding: 10px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+`
+
+const StyledDeleteButton = styled.div`
+    font-weight: bold;    
+    border-radius: 50%;
+    width: 20px;
+    height: 20px;
+    margin-left: 10px;
+    border: 2px solid black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    user-select: none;
+    &:hover{
+      cursor: pointer;
+    }
 `
 
 export function CardC({card}: CardProps) {
+    const [isHovering, setHovered] = useState(false);
+    const toggleIsHovering = () => {
+        setHovered(!isHovering)
+    }
+
     return (
         <React.Fragment>
-            <StyledCard key={card.id}>
-                {card.name}
+            <StyledCard key={card.id} onMouseEnter={toggleIsHovering} onMouseLeave={toggleIsHovering}>
+                <span>{card.name}</span>
+                {isHovering ? <StyledDeleteButton>X</StyledDeleteButton>: null}
             </StyledCard>
         </React.Fragment>
     );
