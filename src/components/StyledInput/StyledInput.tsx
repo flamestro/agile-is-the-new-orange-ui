6aa11e90e1953d06ef78c2e99f11160c78622edf
flamestro <NoReplyFlamestro@gmail.com>
2020-11-22 17:43:09 +0100

    change placeholder color to white, formatting

--- src/components/StyledInput/StyledInput.tsx
+++ src/components/StyledInput/StyledInput.tsx
@@ -1,41 +1,42 @@
 import styled from "styled-components";
-import {grey_4, orange_1} from "../Colors/Colors";
+import {grey_4, orange_1, orange_1_bright, white} from "../Colors/Colors";
 import React from "react";
 
 const InputWithText = styled.input`
-     position: relative;
-     height: 32px;
-     font-weight: bold;
-     text-align: center; 
+    position: relative;
+    height: 32px;
+    font-weight: bold;
+    text-align: center; 
     border-radius: 26px;
     border: 0 solid ${orange_1};
     background: ${orange_1};
     box-shadow:  -6px -6px 12px #cf922d, 
-             6px 6px 12px #ffd643;
+                  6px 6px 12px #ffd643;
+    ::placeholder {
+      color: ${white};
+    }
 `
 
 const StyledFieldSet = styled.fieldset`
-      border:none;
-      position:relative;
-      max-height: 32px;
-
-
+    border:none;
+    position:relative;
+    max-height: 32px;
 `
 
 const StyledInputLabel = styled.fieldset`
-     color: ${grey_4};
-     position: absolute;
-     border: none;
-     top: 3px;
-     left: 15px;
-     right: 0;
-     z-index: 1;
-     pointer-events: none;
-     height: 32px;
-     font-weight: bold;
-     text-align: left; 
-     font-size: 12px;
-     border-radius: 26px;
+    color: ${white};
+    position: absolute;
+    border: none;
+    top: 3px;
+    left: 15px;
+    right: 0;
+    z-index: 1;
+    pointer-events: none;
+    height: 32px;
+    font-weight: bold;
+    text-align: left; 
+    font-size: 12px;
+    border-radius: 26px;
 `
 
 export interface InputProps {
@@ -50,7 +51,12 @@ export function StyledInput({containedText, onChange, placeholderText, formName}
 
         <StyledFieldSet>
             <InputWithText placeholder={placeholderText} type="text" name={formName} value={containedText}
-                           onChange={onChange} onKeyDown={(event) => {if (event.keyCode === 13) {event.preventDefault(); return false;}}}/>
+                           onChange={onChange} onKeyDown={(event) => {
+                if (event.keyCode === 13) {
+                    event.preventDefault();
+                    return false;
+                }
+            }}/>
             {containedText !== "" ? (<StyledInputLabel form={formName}>{placeholderText}</StyledInputLabel>) : null}
         </StyledFieldSet>
     )
