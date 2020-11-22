017dd2a40268f5440abf77b4361f6676dccee195
flamestro <NoReplyFlamestro@gmail.com>
2020-11-22 17:52:18 +0100

    adjust styling on delete button component

--- src/components/StyledDeleteButton/StyledDeleteButton.tsx
+++ src/components/StyledDeleteButton/StyledDeleteButton.tsx
@@ -3,8 +3,8 @@ import styled from "styled-components";
 export const StyledDeleteButton = styled.div`
     font-weight: bold;    
     border-radius: 50%;
-    width: 15px;
-    height: 15px;
+    width: 14px;
+    height: 14px;
     font-size: 10px;
     margin-left: 10px;
     border: 2px solid black;
