9e1003e5bcd835249323925e3e84d64ef07d847c
flamestro <NoReplyFlamestro@gmail.com>
2021-02-27 10:45:34 +0100

    Add new Edit Button

---
+++ src/components/StyledEditButton/StyledEditButton.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+const StyledEditButton = styled.div`
+  font-weight: bold;
+  border-radius: 50%;
+  width: 14px;
+  height: 14px;
+  font-size: 10px;
+  margin-left: 10px;
+  border: 2px solid black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  user-select: none;
+  ::after {
+    content: "Edit";
+  }
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export default StyledEditButton;
